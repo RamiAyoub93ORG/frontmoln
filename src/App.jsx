a3f32dd819a63cd8f1fd8bb1fc2facc222972879
rami ayoub <rami.ayoub93@hotmail.com>
2025-06-08 16:43:16 +0200

    azure build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Eventpage from './assets/pages/Eventpage'
+import EventPage from './assets/pages/EventPage'
 import EventDetailsPage from './assets/pages/EventDetailsPage'
 import BookingEventPage from './assets/pages/BookingEventPage'
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Eventpage/>}/>
+      <Route path="/" element={<EventPage/>}/>
       <Route path="/events/:id" element={<EventDetailsPage/>}/>
       <Route path="/events/booking/:id" element={<BookingEventPage/>}/>
 
